10d481544d40246ae736625e14e85b5b03fff44d
Muskan Chitara <muskan.chitara@sentieo.com>
2021-06-27 17:19:19 +0530

    extracted all the fields

--- sel.py
+++ sel.py
@@ -12,24 +12,20 @@ driver.implicitly_wait(10)
 # go to past events
 driver.find_element(By.XPATH, "//span[contains(@class, 'wd_events_tab_label wd_events_tab_past')]").click()
 
-# extract list of months
-ppt_dates = [i.text for i in driver.find_elements(By.XPATH, "//div[contains(@class, 'item_date wd_event_sidebar_item wd_event_date')]")]
-ppt_times = [i.text for i in driver.find_elements(By.XPATH, "//div[contains(@class, 'item_time wd_event_sidebar_item wd_event_time')]")]
-ppt_titles = [i.text for i in driver.find_elements(By.XPATH, "wd_title")]
-# pdf_links = [i.text for i in driver.find_elements(By.XPATH, "//div[contains(@class, 'wd_events_month_header')]")]
-ppt_summary = [i.text for i in driver.find_elements(By.XPATH, "//div[contains(@class, 'wd_summary')]")]
-pdf_titles = [i.text for i in driver.find_elements(By.XPATH, "//div[contains(@class, 'wd_attachment_title')]")]
-# pdf_links = [i.text for i in driver.find_elements(By.XPATH, "//div[contains(@class, 'wd_events_month_header')]")]
+# extract data
+ppt_dates = ["" or i.get_attribute('innerText')[1:] for i in driver.find_elements(By.CSS_SELECTOR, ".item_date")]
+ppt_times = ["" or i.get_attribute('innerText')[1:] for i in driver.find_elements(By.CSS_SELECTOR, ".item_time")]
+ppt_titles = [i.get_attribute('innerText') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_title")]
+summary = [i.get_attribute('innerText') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_summary")]
 
-# print('title is: ', driver.find_element(By.XPATH, "//div[contains(@class, 'item_time wd_event_sidebar_item wd_event_time')]"))
+attachment = [i.get_attribute('href') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_attachment_title a")]
+print(attachment)
 
-print('length of ppt_dates: ', len(ppt_dates))
-for i in ppt_titles:
-    print('ppt_date is: ', i)
+driver.quit()
 
-# data = []
-# for i in zip(ppt_dates, ppt_times, ppt_titles, ppt_summary, pdf_titles):
-#     data.append(i)
+data = []
+for i in zip(ppt_dates, ppt_times, ppt_titles, summary, attachment):
+    data.append(i)
 
-# df_bs = pd.DataFrame(data, columns=['Date', 'Time', 'Presentation title', 'Presentation summary', 'Attachment (if any)'])
-# df_bs.to_csv('InvestorsPpt.csv')
+df_bs = pd.DataFrame(data, columns=['Date', 'Time (if available)', 'Presentation title', 'Presentation summary (if available)', 'Attachment (if any)'])
+df_bs.to_csv('InvestorsPpt.csv')
