39d33472c5dad30ce46d2026841338cbddd32fab
Muskan Chitara <muskan.chitara@sentieo.com>
2021-06-29 10:54:29 +0530

    error in structured code

--- sel.py
+++ sel.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+from bs4 import BeautifulSoup
 
 url = 'https://investor.weyerhaeuser.com/events-and-presentations'
 
@@ -9,23 +10,73 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
 driver.implicitly_wait(10)
 
+def extractClass(markup, tag_name, class_name, type):
+    data = markup.find(tag_name, {'class': class_name})
+    if type(data) == 'NoneType':
+        return 'NA'
+    elif type != 'text':
+        return data.get_attribute(type)
+    else:
+        return data.get_text()
+
+def extractPptData(markup):
+    date = extractClass(markup, 'div', 'item_date wd_event_sidebar_item wd_event_date', 'text')
+    duration = extractClass(markup, 'div', 'item_time wd_event_sidebar_item wd_event_time', 'text')
+    title = extractClass(markup, 'div', 'wd_event_info', 'text')
+    link = extractClass(markup, 'div', 'wd_event_info', 'href')
+    summary = extractClass(markup, 'div', 'wd_summary', 'text')
+    attachment = extractClass(markup, 'div', 'wd_attachment_title', 'href')
+
+    ppt_data = zip(date, duration, title, link, summary, attachment)
+    return ppt_data
+
+def extractMonthlyData(markup):
+    # pass the string to a BeatifulSoup object
+    soup = BeautifulSoup(markup, 'lxml')
+
+    print('month is: ', soup.select('.wd_events_month_header')[0].text)
+    
+    # extract presentations in given month
+    item_list = soup.find_all('div', {'class': 'item'})
+    monthly_data = []
+
+    for item in item_list:
+        print("************************************new presentation*******************************************")
+        monthly_data.append(extractPptData(item))
+    
+    return monthly_data
+
+def clickOn(xpath):
+    driver.find_element(By.XPATH, xpath).click()
+    return
+
+def extractUsingCSS(attr, css):
+    extracted_list = [i.get_attribute(attr) for i in driver.find_elements(By.CSS_SELECTOR, css)]
+    return extracted_list
+
 # go to past events
-driver.find_element(By.XPATH, "//span[contains(@class, 'wd_events_tab_label wd_events_tab_past')]").click()
+past_events = '//span[contains(@class, "wd_events_tab_label wd_events_tab_past")]'
+clickOn(past_events)
 
-# extract data
-ppt_dates = ["" or i.get_attribute('innerText')[1:] for i in driver.find_elements(By.CSS_SELECTOR, ".item_date")]
-ppt_times = ["" or i.get_attribute('innerText')[1:] for i in driver.find_elements(By.CSS_SELECTOR, ".item_time")]
-ppt_titles = [i.get_attribute('innerText') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_title")]
-summary = [i.get_attribute('innerText') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_summary")]
+#load complete page
+load_more = '//*[@id="wd_printable_content"]/div/button'
+while (True):
+    load_button = driver.find_element(By.XPATH, load_more)
+    status = load_button.get_attribute('style')
+    if status == 'display: none;':
+        break
+    clickOn(load_more)
 
-attachment = [i.get_attribute('href') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_attachment_title a")]
-print(attachment)
+# extract data month-wise
+events_month = [i.get_attribute('innerHTML') for i in driver.find_elements(By.CSS_SELECTOR, ".wd_events_month")]
+
+info = []
+
+for month in events_month:
+    info.append(extractMonthlyData(month))
 
 driver.quit()
 
-data = []
-for i in zip(ppt_dates, ppt_times, ppt_titles, summary, attachment):
-    data.append(i)
-
-df_bs = pd.DataFrame(data, columns=['Date', 'Time (if available)', 'Presentation title', 'Presentation summary (if available)', 'Attachment (if any)'])
+# store extracted data in csv format
+df_bs = pd.DataFrame(info, columns=['Date', 'Time (if available)', 'Presentation title', 'Presentation link', 'Presentation summary (if available)', 'Attachment (if any)'])
 df_bs.to_csv('InvestorsPpt.csv')
